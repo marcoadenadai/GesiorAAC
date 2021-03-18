38ffe4f75b1e9906f311b5e2c6cf7da62c3c3f5d
Marco <Marco@DESKTOP-82HB7MH>
2021-03-18 18:14:17 -0300

    test

--- install.php
+++ install.php
@@ -344,7 +344,13 @@ elseif($page == 'step')
 		$samplePlayers[3] = 'Paladin Sample';
 		$samplePlayers[4] = 'Knight Sample';
 
-		$account = new Account(1, Account::LOADTYPE_NAME);
+		$account = new Account(1, Account::LOADTYPE_ID);
+		function console_log( $data ){
+			echo '<script>';
+			echo 'console.log('. json_encode( $data ) .')';
+			echo '</script>';
+		  }
+		console_log($account->isLoaded());
 		if(!$account->isLoaded())
 		{
 		    $account = new Account();
